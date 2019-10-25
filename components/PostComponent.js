a74aa6f8549ebd33eae2ce90ff0045340b3d30d7
Gokay Okyay <gokayokyay@hotmail.com>
2019-10-25 19:21:32 +0000

    Added PostComponent

---
+++ components/PostComponent.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+class PostComponent extends React.Component {
+    render() {
+        return (
+            <View style={[styles.root, this.props.style]}>
+                <View onTouchEnd={(e) => console.log('pressed')} style={styles.container}>
+                    <View style={styles.top.container}>
+                        <View style={styles.top.textView.container}>
+                            <Text>Just burned 2000 calories. That's the last time I leave brownies in the oven while I sleep. :)</Text>
+                        </View>
+                        <View style={styles.top.voteView.container}>
+                            <TouchableOpacity style={styles.top.voteView.upArrow}>
+                                <Ionicons name="ios-arrow-up" size={32} color="black" />
+                            </TouchableOpacity>
+                            <View style={styles.top.voteView.voteCount.container}>
+                                <Text style={styles.top.voteView.voteCount.text}>24</Text>
+                            </View>
+                            <TouchableOpacity style={styles.top.voteView.downArrow}>
+                                <Ionicons name="ios-arrow-down" size={32} color="black" />    
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View style={styles.bottom.container}>
+                        <View style={styles.bottom.leftView.container}>
+                            <Text style={styles.bottom.leftView.text}>6 minutes ago</Text>
+                        </View>
+                        <View style={styles.bottom.rightView.container}>
+                            <Text style={styles.bottom.rightView.text}>3 replies</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = {
+    root: {
+        flex: 1,
+        padding: '5%',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 7,
+        
+        elevation: 4,
+
+        maxHeight: 180
+    },
+    container: {
+        flex: 1,
+        width: '100%',
+        borderWidth: 1,
+        borderColor: '#ededed',
+        borderRadius: 8,
+        backgroundColor: 'white',
+        // padding: '3%',
+
+    },
+    top: {
+        container: {
+            flex: 3,
+            height: '100%',
+            width: '100%',
+            // backgroundColor: 'red',
+            flexDirection: 'row',
+            padding: '3%'
+        },
+        textView: {
+            container: {
+                flex: 8,
+                height: '100%',
+                width: '100%',
+            },
+            text: {
+
+            }
+        },
+        voteView: {
+            container: {
+                flex: 2,
+                height: '100%',
+                width: '100%',
+                marginTop: '-3%',
+                marginRight: '-3%'
+            },
+            upArrow: {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            },
+            voteCount: {
+                container: {
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                },
+                text: {
+                    fontSize: 20,
+                    fontWeight: 'bold'
+                }
+            },
+            downArrow: {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }
+        }
+    },
+    bottom: {
+        container: {
+            flex: 1,
+            height: '100%',
+            width: '100%',
+            flexDirection: 'row',
+        },
+        leftView: {
+            container: {
+                flex: 1,
+                alignItems: 'flex-start',
+                justifyContent: 'center',
+                paddingLeft: '3%'
+            },
+            text: {
+                color: 'gray',
+                fontSize: 10
+            }
+        },
+        rightView: {
+            container: {
+                flex: 1,
+                alignItems: 'flex-end',
+                justifyContent: 'center',
+                paddingRight: '3%'
+            },
+            text: {
+                color: 'black',
+                fontSize: 10,
+                fontWeight: 'bold'
+            }
+        }
+    }
+}
+
+export default PostComponent;
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView, FlatList } from 'react-native';
 import CreatePostComponent from '../components/CreatePostComponent';
 import FeedSelectionComponent from '../components/FeedSelectionComponent';
+import PostComponent from '../components/PostComponent';
+
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
 
 class MainScreen extends React.Component {
     render() {
@@ -11,7 +27,24 @@ class MainScreen extends React.Component {
                 <FeedSelectionComponent />
                 <CreatePostComponent />
             </View>
-            <View style={styles.bottomView}></View>
+            <View style={styles.bottomView}>
+                {/* <PostComponent /> */}
+                <FlatList
+                    data={DATA}
+                    renderItem={(item) => {
+                        <View style={{height: 300, width: 300}}>
+                            <PostComponent />
+                        </View>
+                    }}
+                    contentContainerStyle={{
+                        flex: 1,
+                        width: 100,
+                        height: 100
+                    }}
+                    style={{flex: 1, height: '100%', width: '100%'}}
+                    keyExtractor={item=>item.id}
+                />
+            </View>
         </View>
       );
     }
@@ -38,7 +71,8 @@ const styles = {
     bottomView: {
         flex: 8,
         height: '80%',
-        width: '100%'
+        width: '100%',
+        paddingTop: 20
     }
 }
 
